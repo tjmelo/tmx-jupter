cab003b8b36607cbe9c70a2b25e78ec163951527
Thiago Melo <thiago.tmwd@gmail.com>
2022-03-24 20:19:00 -0300

    Redefining control state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.sass'
 import type { AppProps } from 'next/app'
+import { store } from '../model'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,26 @@ import styles from '../styles/Home.module.sass'
 import { data } from '../controller'
 import { Input } from '../components/input'
 
-import { store } from '../model'
-import { Provider } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 interface Data {
   data: object
 }
 
 const Home: NextPage<Data> = ({data}) => {
+  const CEP = useSelector((state) => state.CEP.value)
+  
   console.log(data)
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Find any address through a CEP</h1>
-      <Provider store={store}>
         <Input />
-      </Provider>
     </div>
   )
+   
 }
 
-export const getStaticProps = async () => data('31610220')
+export const getStaticProps = async () => data('0')
 
 export default Home
